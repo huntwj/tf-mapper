01b03684e03ba387a15e89d655f870dc1a04528b
Wil Hunt <huntwj@gmail.com>
2013-04-05 21:10:35 -0700

    Add warning level log messages.

--- src/main.cc
+++ src/main.cc
@@ -9,11 +9,13 @@ int main(int argc, char *argv[])
 {
     google::InitGoogleLogging(argv[0]);
     DLOG(INFO) << "stderr";
+    DLOG(WARNING) << "stderr";
     FLAGS_logtostderr = 1;
+    FLAGS_minloglevel = 1;
 
     tf_mapper::App app;
     
-    DLOG(INFO) << "Hello, " << argv[0] << " World!";
+    DLOG(WARNING) << "Hello, " << argv[0] << " World!";
     return 0;
 }
 
