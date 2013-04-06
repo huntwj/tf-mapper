28a2c74dd7f585359ccbd4bda2f4370a2e1af2ad
Wil Hunt <huntwj@gmail.com>
2013-04-05 23:36:42 -0700

    Create initial skeleton for LogFileImporter.

---
+++ src/LogFileImporter.cc
@@ -0,0 +1,9 @@
+#include "LogFileImporter.h"
+
+namespace tf_mapper
+{
+    LogFileImporter::LogFileImporter(std::string p_fileName)
+    {
+
+    }
+}
---
+++ src/LogFileImporter.h
@@ -0,0 +1,19 @@
+#ifndef TF_MAPPER_LOGFILEIMPORTER_H_
+#define TF_MAPPER_LOGFILEIMPORTER_H_
+
+#include <string>
+
+namespace tf_mapper {
+
+    class LogFileImporter
+    {
+        public:
+            /**
+             * Construct a LogFileImporter for a given file.
+             */
+            LogFileImporter(std::string p_fileName);
+    }
+
+}
+
+#endif
